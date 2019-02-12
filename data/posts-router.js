2ffa1a023dd1f624fd8ec2476a58de7efbb81d09
Olivia Osborn <olioz95@gmail.com>
2019-02-12 17:01:50 -0500

    created get request for posts/:id

--- data/posts-router.js
+++ data/posts-router.js
@@ -7,8 +7,21 @@ router.get("/", async (req, res) => {
         const posts = await Posts.find(req.query);
         res.status(200).json(posts)
     } catch (error) {
+        res.status(500).json({ error: "The posts information could not be retrieved." })
+    }
+})
+
+router.get("/:id", async (req, res) => {
+    try {
+        const post = await Posts.findById(req.params.id);
+        if (post) {
+            res.status(200).json(post)
+        } else {
+            res.status(404).json({ message: "The post with the specified ID does not exist."})
+        }
+    } catch (error) {
         console.log(error)
-        res.status(500).json({message: "Error retrieving the posts"})
+        res.status(500).json({ error: "The post information could not be retrieved." })
     }
 })
 
